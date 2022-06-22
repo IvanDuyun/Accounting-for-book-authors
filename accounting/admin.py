4b3b10c602a8929709337751e6ee8b82eaa5e9ad
IvanDuyun <Duyun90031@gmail.com>
2022-06-22 20:51:57 +0300

    problem solving n+1

--- accounting/admin.py
+++ accounting/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.db.models import Count
+
 from .models import Author, Book
 
 class BookInline(admin.TabularInline):
@@ -11,10 +13,14 @@ class PersonAuthor(admin.ModelAdmin):
         BookInline,
     ]
 
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.annotate(books_count=Count('book'))
+
     def show_number_of_books(self, obj):
         from django.utils.html import format_html
 
-        result = Book.objects.filter(author=obj).count()
+        result = obj.books_count
         return format_html("<b><i>{}</i></b>", result)
     show_number_of_books.short_description = "Количество книг"
 
